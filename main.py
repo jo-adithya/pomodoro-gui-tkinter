1dc92a18538ba051db15ae378e92e21297cfc060
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-14 10:26:16 +0700

    add a countdown mechanism

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    countdown(300)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+    minutes = count // 60
+    seconds = count % 60
+    canvas.itemconfig(timer, text=f'{str(minutes).zfill(2)}:{str(seconds).zfill(2)}')
+    if count > 0:
+        window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,7 +31,7 @@ window.config(padx=100, pady=50, bg=BG)
 canvas = Canvas(width=200, height=224, bg=BG, highlightthickness=0)
 image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 133, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer = canvas.create_text(100, 133, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(row=1, column=1)
 
 title = Label(text='Timer', font=(FONT_NAME, 50, 'normal'), bg=BG, fg=GREEN)
@@ -31,7 +39,7 @@ check = Label(text='✔︎', font=(FONT_NAME, 20, 'normal'), bg=BG, fg=GREEN)
 title.grid(row=0, column=1)
 check.grid(row=3, column=1)
 
-start_btn = Button(text='Start', highlightthickness=0)
+start_btn = Button(text='Start', highlightthickness=0, command=start_timer)
 reset_btn = Button(text='Reset', highlightthickness=0)
 start_btn.grid(row=2, column=0)
 reset_btn.grid(row=2, column=2)
