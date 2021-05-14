ee6925cbb91b0dcb2b08f6e809282100b9f769b3
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-14 10:01:26 +0700

    set up the user interface

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+PINK = "#eec4c4"
+RED = "#ffaaa7"
+GREEN = "#98ddca"
+BG = "#f7f3e9"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -16,3 +16,24 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=BG)
+
+canvas = Canvas(width=200, height=224, bg=BG, highlightthickness=0)
+image = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 133, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(row=1, column=1)
+
+title = Label(text='Timer', font=(FONT_NAME, 50, 'normal'), bg=BG, fg=GREEN)
+check = Label(text='✔︎', font=(FONT_NAME, 20, 'normal'), bg=BG, fg=GREEN)
+title.grid(row=0, column=1)
+check.grid(row=3, column=1)
+
+start_btn = Button(text='Start', highlightthickness=0)
+reset_btn = Button(text='Reset', highlightthickness=0)
+start_btn.grid(row=2, column=0)
+reset_btn.grid(row=2, column=2)
+
+window.mainloop()
